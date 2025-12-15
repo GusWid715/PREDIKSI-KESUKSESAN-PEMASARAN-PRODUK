7afd3732a789ae2db9dc92315957e1fca9311d0d
GusWid715 <bawaskara30@gmail.com>
2025-12-15 08:40:37 +0800

    import library

---
+++ NB.py
@@ -0,0 +1,9 @@
+# 1. Import Pustaka yang dibutuhkan
+# Untuk membuat tampilan Web App
+import streamlit as st          
+# Pandas untuk membuat tabel data
+import pandas as pd             
+# Algoritma Naive Bayes (Gaussian)
+from sklearn.naive_bayes import GaussianNB 
+# Preprocessing untuk mengubah kata menjadi angka
+from sklearn import preprocessing 
