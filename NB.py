6bbafd677b29039ac03509de63986671617dd815
GusWid715 <bawaskara30@gmail.com>
2025-12-15 08:42:21 +0800

    import data

--- NB.py
+++ NB.py
@@ -6,4 +6,17 @@ import pandas as pd
 # Algoritma Naive Bayes (Gaussian)
 from sklearn.naive_bayes import GaussianNB 
 # Preprocessing untuk mengubah kata menjadi angka
-from sklearn import preprocessing 
+from sklearn import preprocessing
+
+# 2. Menyiapkan Data 
+data = {
+    'Iklan': ['Ya', 'Tidak', 'Ya', 'Tidak'],
+    'Harga': ['Rendah', 'Tinggi', 'Sedang', 'Rendah'],
+    'Kesuksesan': ['Berhasil', 'Tidak Berhasil', 'Berhasil', 'Tidak Berhasil']
+}
+
+# Membuat tabel (DataFrame) agar rapi
+df = pd.DataFrame(data)
+
+st.subheader("1. Data Awal")
+st.dataframe(df, use_container_width=True)
